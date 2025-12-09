75b26196181d1f9376ca07e4a1e42d5ec054c875
Carlotta Fischer <carlotta.fischer@stud.uni-heidelberg.de>
2025-12-09 18:38:08 +0100

    ReviewController methods implemented

--- api/src/main/java/de/seuhd/campuscoffee/api/controller/ReviewController.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/controller/ReviewController.java
@@ -5,6 +5,7 @@ import de.seuhd.campuscoffee.api.mapper.DtoMapper;
 import de.seuhd.campuscoffee.api.openapi.CrudOperation;
 import de.seuhd.campuscoffee.domain.model.objects.Review;
 import de.seuhd.campuscoffee.domain.ports.api.CrudService;
+import de.seuhd.campuscoffee.domain.ports.api.ReviewService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -29,24 +30,41 @@ import static de.seuhd.campuscoffee.api.openapi.Resource.REVIEW;
 @RequiredArgsConstructor
 public class ReviewController extends CrudController<Review, ReviewDto, Long> {
 
-    // TODO: Correctly implement the service() and mapper() methods. Note the IntelliJ warning resulting from the @NonNull annotation.
+
+    private final ReviewService reviewService;
+    private final DtoMapper<Review, ReviewDto> reviewMapper;
 
     @Override
     protected @NonNull CrudService<Review, Long> service() {
-        return null;
+        return reviewService;
     }
 
     @Override
     protected @NonNull DtoMapper<Review, ReviewDto> mapper() {
-        return null;
+        return reviewMapper;
     }
 
     @Operation
     @CrudOperation(operation=GET_ALL, resource=REVIEW)
     @GetMapping("")
-    public @NonNull ResponseEntity<List<ReviewDto>> getAll() {
-        return super.getAll();
-    }
 
-    // TODO: Implement the missing methods/endpoints.
+    public @NonNull ResponseEntity<List<ReviewDto>> getAll() {return super.getAll();}
+    public @NonNull ResponseEntity <ReviewDto> getById (@PathVariable Long id) {return super.getById(id);}
+    public @NonNull ResponseEntity < ReviewDto > create (@PathVariable ReviewDto dto) {return super.create(dto);}
+    public @NonNull ResponseEntity < ReviewDto > update (@PathVariable Long id, @PathVariable ReviewDto dto) {return super.update(id,dto);}
+    public @NonNull ResponseEntity <Void > delete (@PathVariable Long id) {return super.delete(id);}
+    public ResponseEntity <List<ReviewDto>> filter(@RequestParam(" pos_id ") Long posId, @RequestParam(" approved ") Boolean approved)
+    {
+        List<Review> reviews = reviewService.filter(posId, approved);
+        List<ReviewDto> result = reviews.stream().map(reviewMapper::fromDomain).toList();
+        return ResponseEntity.ok(result);
+    }
+    public ResponseEntity <ReviewDto> approve (@PathVariable Long id , @RequestParam (" user_id ") Long userId)
+      {
+          Review review = reviewService.getById(id);
+          Review approved = reviewService.approve(review, userId);
+          ReviewDto dto = reviewMapper.fromDomain(approved);
+          return ResponseEntity.ok(dto);
+      }
+
 }
